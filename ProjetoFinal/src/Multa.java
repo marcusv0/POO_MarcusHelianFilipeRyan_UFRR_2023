f1abc9d850d03a33a2ddae65679e15fa1fbb0fb7
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-12 21:39:13 -0400

    Update Multa.java

--- ProjetoFinal/src/Multa.java
+++ ProjetoFinal/src/Multa.java
@@ -1,8 +1,11 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.*;
 
 public class Multa {
+    private static int contadorMulta = 1;
+    private int id;
     private int valor;
     private Cliente cliente;
     private Carro carro;
@@ -20,21 +23,36 @@ public class Multa {
     public String getCnh() {
         return cnh;
     }
-
     public void setCnh(String cnh) {
         this.cnh = cnh;
     }
 
-    public void registrarMulta() {
-        // Lógica para registrar a multa...
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
 
-        // Agora, vamos salvar os dados no arquivo txt
+    public void registrarMulta(int id, int valor, String cnh, Cliente cliente, Carro carro, String descricao) {
+        this.id = id;
+        this.valor = valor;
+        this.cnh = cnh;
+        this.cliente = cliente;
+        this.carro = carro;
+        this.descricao = descricao;
         salvarDadosEmArquivo();
+    }
+
+    public void cancelarMulta(int id) {
+        removerDadosDoArquivo(id);
+        System.out.println("Multa cancelada com sucesso!");
     }
 
     private void salvarDadosEmArquivo() {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("dados_multas.txt", true))) {
             // Adiciona os dados ao arquivo
+            writer.write("ID: " + id + "\n");
             writer.write("Cliente: " + cliente.getNome() + "\n");
             writer.write("Carro: " + carro.getModelo() + "\n");
             writer.write("CNH: " + cnh + "\n");
@@ -45,6 +63,39 @@ public class Multa {
             System.out.println("Dados salvos no arquivo com sucesso!");
         } catch (IOException e) {
             System.err.println("Erro ao salvar os dados no arquivo: " + e.getMessage());
+        }
+    }
+
+    private void removerDadosDoArquivo(int id) {
+        try {
+            // Lê o conteúdo do arquivo
+            BufferedReader reader = new BufferedReader(new FileReader("dados_multas.txt"));
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                // Verifica se a linha contém o ID da multa a ser removida
+                if (line.contains("ID: " + id)) {
+                    // Pula as linhas da multa a ser removida
+                    for (int i = 0; i < 6; i++) {
+                        reader.readLine();
+                    }
+                } else {
+                    // Adiciona a linha ao buffer
+                    stringBuffer.append(line).append("\n");
+                }
+            }
+
+            reader.close();
+
+            // Escreve o conteúdo modificado de volta no arquivo
+            BufferedWriter writer = new BufferedWriter(new FileWriter("dados_multas.txt"));
+            writer.write(stringBuffer.toString());
+            writer.close();
+
+            System.out.println("Dados removidos do arquivo com sucesso!");
+        } catch (IOException e) {
+            System.err.println("Erro ao remover os dados do arquivo: " + e.getMessage());
         }
     }
 }
