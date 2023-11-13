eabee13d61807649d15381c2b270f0a65adc8feb
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-12 22:01:42 -0400

    Update Faturas.java

--- ProjetoFinal/src/Faturas.java
+++ ProjetoFinal/src/Faturas.java
@@ -1,5 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Faturas {
     private Reserva reserva;
     private double valorTotal;
-    private itemFatura;
+    private List<itemFatura> item;
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public void adicionarItemFatura(itemFatura itemFatura) {
+        if (item == null) {
+            item = new ArrayList<>();
+        }
+        item.add(itemFatura);
+    }
+
+    public List<itemFatura> getItem() {
+        return item;
+    }
+
 }
