540d8453260a6164494f7dbaa1a8a866317ee7cf
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-13 10:52:18 -0400

    Update Locadora.java

--- ProjetoFinal/src/Locadora.java
+++ ProjetoFinal/src/Locadora.java
@@ -1,3 +1,35 @@
 public class Locadora {
-
-}
+    private String nome;
+    private String cnpj;
+    private String endereco;
+    private String telefone;
+    private Funcionario funcionario;
+    
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
+    public String getCnpj(){
+        return cnpj;
+    }
+    public void setCnpj(String cnpj){
+        this.cnpj = cnpj;
+    }
+    
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+    
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+    
