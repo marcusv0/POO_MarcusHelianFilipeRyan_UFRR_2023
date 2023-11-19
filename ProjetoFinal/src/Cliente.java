90b5bd3c4f3a06ef3e4d1b77023cb20cfabbaecf
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-19 19:44:29 -0400

    Update Cliente.java
    Foi adicionado um novo atributo idade, junto com seu getter e seu setter, além de um método para registrar o cliente na locadora, podendo ser modificado no futuro, e o método para verificar se o cliente está apto a alugar um carro, podendo ser modificado no futuro também.

--- ProjetoFinal/src/Cliente.java
+++ ProjetoFinal/src/Cliente.java
@@ -1,9 +1,10 @@
 public class Cliente {
-    protected String nome;
-    protected String rg;
-    protected String endereco;
-    protected String telefone;
-    protected  String cnh;
+    private String nome;
+    private String rg;
+    private String endereco;
+    private String telefone;
+    private String cnh;
+    private int idade;
 
     public String getNome(){
         return nome;
@@ -38,6 +39,29 @@ public class Cliente {
     }
     public void setCnh(String cnh){
         this.cnh = cnh;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
+
+    public void registrarCliente(String nome, String rg, String endereco, String telefone, String cnh) {
+        this.nome = nome;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cnh = cnh;
+    }
+
+    public boolean estaAptoParaAlugarCarro() {
+        if (this.getIdade() < 18) {
+            return false;
+        }
+        return true;
     }
 
 }
