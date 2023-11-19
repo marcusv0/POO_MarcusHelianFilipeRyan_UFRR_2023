88621f622cdd94df0a90b64c756f7c999810747d
Filipetom04 <142317897+Filipetom04@users.noreply.github.com>
2023-11-19 19:50:07 -0400

    comentando...

--- ProjetoFinal/src/Carro.java
+++ ProjetoFinal/src/Carro.java
@@ -1,41 +1,79 @@
+/**
+ * Classe que representa um carro na locadora.
+ * Contém informações como modelo, placa e marca do carro.
+ */
 public class Carro {
+    // Atributos protegidos para armazenar informações do carro
     protected String modelo;
     protected String placa;
     protected String marca;
 
+    /**
+     * Construtor que inicializa um objeto Carro com as informações fornecidas.
+     * @param modelo O modelo do carro.
+     * @param placa A placa do carro.
+     * @param marca A marca do carro.
+     */
     public Carro(String modelo, String placa, String marca) {
         this.modelo = modelo;
         this.placa = placa;
         this.marca = marca;
     }
 
-    public String getModelo(){
+    /**
+     * Obtém o modelo do carro.
+     * @return O modelo do carro.
+     */
+    public String getModelo() {
         return modelo;
     }
 
-    public void setModelo(String modelo){
+    /**
+     * Define o modelo do carro.
+     * @param modelo O novo modelo do carro.
+     */
+    public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    public String getPlaca(){
+    /**
+     * Obtém a placa do carro.
+     * @return A placa do carro.
+     */
+    public String getPlaca() {
         return placa;
     }
 
-    public void setPlaca(String placa){
+    /**
+     * Define a placa do carro.
+     * @param placa A nova placa do carro.
+     */
+    public void setPlaca(String placa) {
         this.placa = placa;
     }
 
-    public String getMarca(){
+    /**
+     * Obtém a marca do carro.
+     * @return A marca do carro.
+     */
+    public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca){
+    /**
+     * Define a marca do carro.
+     * @param marca A nova marca do carro.
+     */
+    public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     * Método de teste que imprime os atributos do carro.
+     */
     public void testeAtributos() {
-        System.out.println(this.marca);
-        System.out.println(this.modelo);
-        System.out.println(this.placa);
+        System.out.println("Marca: " + this.marca);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Placa: " + this.placa);
     }
 }
