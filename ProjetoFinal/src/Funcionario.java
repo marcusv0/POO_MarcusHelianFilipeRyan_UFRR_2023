2aa8a658bd27da89e5e36560885049b729e997ed
Filipetom04 <142317897+Filipetom04@users.noreply.github.com>
2023-11-19 22:46:14 -0400

    Update Funcionario.java

--- ProjetoFinal/src/Funcionario.java
+++ ProjetoFinal/src/Funcionario.java
@@ -1,35 +1,71 @@
 public class Funcionario {
+    // Atributos privados para armazenar informações do funcionário
     private String nome;
     private String rg;
     private String endereco;
     private String telefone;
 
-    public String getNome(){
+    /**
+     * Obtém o nome do funcionário.
+     * @return O nome do funcionário.
+     */
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+
+    /**
+     * Define o nome do funcionário.
+     * @param nome O novo nome do funcionário.
+     */
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getRg(){
+    /**
+     * Obtém o RG do funcionário.
+     * @return O RG do funcionário.
+     */
+    public String getRg() {
         return rg;
     }
-    public void setRg(String rg){
+
+    /**
+     * Define o RG do funcionário.
+     * @param rg O novo RG do funcionário.
+     */
+    public void setRg(String rg) {
         this.rg = rg;
     }
 
-    public String getEndereco(){
+    /**
+     * Obtém o endereço do funcionário.
+     * @return O endereço do funcionário.
+     */
+    public String getEndereco() {
         return endereco;
     }
-    public void setEndereco(String endereco){
+
+    /**
+     * Define o endereço do funcionário.
+     * @param endereco O novo endereço do funcionário.
+     */
+    public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 
-    public String getTelefone(){
+    /**
+     * Obtém o telefone do funcionário.
+     * @return O telefone do funcionário.
+     */
+    public String getTelefone() {
         return telefone;
     }
-    public void setTelefone(String telefone){
+
+    /**
+     * Define o telefone do funcionário.
+     * @param telefone O novo telefone do funcionário.
+     */
+    public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-    
 }
