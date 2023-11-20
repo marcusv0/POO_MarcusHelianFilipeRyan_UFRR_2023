fade455ee81603a401d11f8f5309dc273f653d3d
Filipetom04 <142317897+Filipetom04@users.noreply.github.com>
2023-11-19 22:48:15 -0400

    Update itemFatura.java

--- ProjetoFinal/src/itemFatura.java
+++ ProjetoFinal/src/itemFatura.java
@@ -1,19 +1,36 @@
 public class itemFatura {
+    // Atributos privados para armazenar informações do item da fatura
     private String descricao;
     private double valor;
 
+    /**
+     * Obtém a descrição do item da fatura.
+     * @return A descrição do item da fatura.
+     */
     public String getDescricao() {
         return descricao;
     }
 
+    /**
+     * Define a descrição do item da fatura.
+     * @param descricao A nova descrição do item da fatura.
+     */
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
 
+    /**
+     * Obtém o valor do item da fatura.
+     * @return O valor do item da fatura.
+     */
     public double getValor() {
         return valor;
     }
 
+    /**
+     * Define o valor do item da fatura.
+     * @param valor O novo valor do item da fatura.
+     */
     public void setValor(double valor) {
         this.valor = valor;
     }
