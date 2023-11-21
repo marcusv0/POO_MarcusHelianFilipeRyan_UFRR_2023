da1bc224ef3fa6e7e92b2794734ba7700464ea0f
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-21 19:00:25 -0400

    Update Main.java

--- ProjetoFinal/src/Main.java
+++ ProjetoFinal/src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
     public static void main(String[] args){
-        Carro carroteste1 = new Carro("Triton", "54354", "Mitsubish");
-        carroteste1.testeAtributos();
-        carroteste1.salvarCarro();
-
-        Carro carroteste2 =  new Carro("Civic", "f12345", "Honda");
-        carroteste2.testeAtributos();
-        carroteste2.salvarCarro();
+        Cliente cliente1 = new Cliente("André", "25539", "Av. Venezuela", "95-99948-7633", "962488", 23);
+        cliente1.registrarCliente();
+        Carro carro1 = new Carro("ModeloXYZ", "ABC123", "MarcaXYZ");
+        carro1.salvarCarro();
+        Multa multa1 = new Multa(150, cliente1, carro1, "Excesso de velocidade");
+        multa1.registrarMulta();
+        Data data1 = new Data(1, 1, 2022);
+        Data data2 = new Data(10, 11, 2023);
+        Reserva reserva1 = new Reserva(cliente1, data1, data2, carro1);
+        reserva1.salvarReservaEmArquivo();
         
-        System.out.println(Carro.getQuantidade()); // retorna a quantidade de carros
+        Cliente cliente2 = new Cliente("Pedro", "8539", "Av. Ville Roy", "98-9948-7633", "712488", 30);
+        cliente2.registrarCliente();
+        Carro carro2 = new Carro("ModelYZX", "DEF456", "MarcaYZX");
+        carro2.salvarCarro();
+        Multa multa2 = new Multa(350, cliente2, carro2, "Atravessar sinal vermelho");
+        multa2.registrarMulta();
+        Data data3 = new Data(18, 11, 2022);
+        Data data4 = new Data(10, 8, 2023);
+        Reserva reserva2 = new Reserva(cliente2, data3, data4, carro2);
+        reserva2.salvarReservaEmArquivo();
+
+
     }
 }
