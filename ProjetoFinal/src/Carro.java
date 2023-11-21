261865aef76d25bfbb6c9c389c2656d7ced135c4
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-21 19:28:50 -0400

    Update Carro.java

--- ProjetoFinal/src/Carro.java
+++ ProjetoFinal/src/Carro.java
@@ -1,94 +1,53 @@
-/**
- * Classe que representa um carro na locadora.
- * Contém informações como modelo, placa e marca do carro.
- */
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Carro {
-    // Atributos protegidos para armazenar informações do carro
     protected String modelo;
     protected String placa;
     protected String marca;
     private static int quantidade=0;
 
-    /**
-     * Construtor que inicializa um objeto Carro com as informações fornecidas.
-     * @param modelo O modelo do carro.
-     * @param placa A placa do carro.
-     * @param marca A marca do carro.
-     */
     public Carro(String modelo, String placa, String marca) {
         this.modelo = modelo;
         this.placa = placa;
         this.marca = marca;
-        quantidade++; // adiciona 1 à quantidade
+        quantidade++;
     }
 
-    /**
-     * Obtém o modelo do carro.
-     * @return O modelo do carro.
-     */
     public String getModelo() {
         return modelo;
     }
 
-    /**
-     * Define o modelo do carro.
-     * @param modelo O novo modelo do carro.
-     */
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-    /**
-     * Obtém a placa do carro.
-     * @return A placa do carro.
-     */
     public String getPlaca() {
         return placa;
     }
 
-    /**
-     * Define a placa do carro.
-     * @param placa A nova placa do carro.
-     */
     public void setPlaca(String placa) {
         this.placa = placa;
     }
 
-    /**
-     * Obtém a marca do carro.
-     * @return A marca do carro.
-     */
     public String getMarca() {
         return marca;
     }
 
-    /**
-     * Define a marca do carro.
-     * @param marca A nova marca do carro.
-     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
-    /**
-     * Método de teste que imprime os atributos do carro.
-     */
     public void testeAtributos() {
         System.out.println("Marca: " + this.marca);
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Placa: " + this.placa);
     }
 
-    /**
-     * Método que salva os dados do carro em um arquivo txt
-     */
     public void salvarCarro() {
-        String caminho = new String("dadosCarro" + File.separator + "carro" + Carro.getQuantidade()); 
+        String caminho = new String("dadosCarro" + File.separator + "carro" + Carro.getQuantidade());
         try {
             FileWriter arquivo = new FileWriter(caminho, true); // se não existe, cria
             BufferedWriter escritor = new BufferedWriter(arquivo);
@@ -96,19 +55,34 @@ public class Carro {
             escritor.write(this.getModelo());
             escritor.write("\n" + this.getMarca());
             escritor.write("\n" + this.getPlaca());
-            
+
 
             escritor.close();
-            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * Método que retorna a quantidade de carros criados(atributo da classe)
-     * @return A quantidade de carros
-     */
+    public void excluirDadosDoArquivo() {
+        String caminho = new String("dadosCarro" + File.separator + "carro" + Carro.getQuantidade());
+        try {
+            File arquivo = new File(caminho);
+
+            if (arquivo.exists()) {
+                if (arquivo.delete()) {
+                    System.out.println("Dados removidos do arquivo com sucesso!");
+                } else {
+                    System.err.println("Erro ao remover o arquivo.");
+                }
+            } else {
+                System.out.println("Arquivo não encontrado.");
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao excluir dados do arquivo: " + e.getMessage());
+        }
+    }
+
     public static int getQuantidade() {
         return quantidade;
     }
