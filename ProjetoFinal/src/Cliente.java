3367144ff18b78016a055ed318c67a2b18d79801
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-21 18:58:59 -0400

    Update Cliente.java

--- ProjetoFinal/src/Cliente.java
+++ ProjetoFinal/src/Cliente.java
@@ -1,8 +1,9 @@
-/**
- * Classe que representa um cliente da locadora de carros.
- */
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Cliente {
-    // Atributos privados para armazenar informações do cliente
     private String nome;
     private String rg;
     private String endereco;
@@ -10,123 +11,83 @@ public class Cliente {
     private String cnh;
     private int idade;
 
-    /**
-     * Obtém o nome do cliente.
-     * @return O nome do cliente.
-     */
-    public String getNome() {
+    public Cliente(String nome, String rg, String endereco, String telefone, String cnh, int idade) {
+        this.nome = nome;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cnh = cnh;
+        this.idade = idade;
+    }
+    
+    public String getNome(){
         return nome;
     }
-
-    /**
-     * Define o nome do cliente.
-     * @param nome O novo nome do cliente.
-     */
-    public void setNome(String nome) {
+    public void setNome(String nome){
         this.nome = nome;
     }
 
-    /**
-     * Obtém o RG do cliente.
-     * @return O RG do cliente.
-     */
-    public String getRg() {
+    public String getRg(){
         return rg;
     }
-
-    /**
-     * Define o RG do cliente.
-     * @param rg O novo RG do cliente.
-     */
-    public void setRg(String rg) {
+    public void setRg(String rg){
         this.rg = rg;
     }
 
-    /**
-     * Obtém o endereço do cliente.
-     * @return O endereço do cliente.
-     */
-    public String getEndereco() {
+    public String getEndereco(){
         return endereco;
     }
-
-    /**
-     * Define o endereço do cliente.
-     * @param endereco O novo endereço do cliente.
-     */
-    public void setEndereco(String endereco) {
+    public void setEndereco(String endereco){
         this.endereco = endereco;
     }
 
-    /**
-     * Obtém o telefone do cliente.
-     * @return O telefone do cliente.
-     */
-    public String getTelefone() {
+    public String getTelefone(){
         return telefone;
     }
-
-    /**
-     * Define o telefone do cliente.
-     * @param telefone O novo telefone do cliente.
-     */
-    public void setTelefone(String telefone) {
+    public void setTelefone(String telefone){
         this.telefone = telefone;
     }
 
-    /**
-     * Obtém a CNH (Carteira Nacional de Habilitação) do cliente.
-     * @return A CNH do cliente.
-     */
-    public String getCnh() {
+    public String getCnh(){
         return cnh;
     }
-
-    /**
-     * Define a CNH (Carteira Nacional de Habilitação) do cliente.
-     * @param cnh A nova CNH do cliente.
-     */
-    public void setCnh(String cnh) {
+    public void setCnh(String cnh){
         this.cnh = cnh;
     }
 
-    /**
-     * Obtém a idade do cliente.
-     * @return A idade do cliente.
-     */
-    public int getIdade() {
+    public int getIdade(){
         return idade;
     }
 
-    /**
-     * Define a idade do cliente.
-     * @param idade A nova idade do cliente.
-     */
-    public void setIdade(int idade) {
+    public void setIdade(int idade){
         this.idade = idade;
     }
 
-    /**
-     * Registra as informações básicas do cliente.
-     * @param nome     O nome do cliente.
-     * @param rg       O RG do cliente.
-     * @param endereco O endereço do cliente.
-     * @param telefone O telefone do cliente.
-     * @param cnh      A CNH do cliente.
-     */
-    public void registrarCliente(String nome, String rg, String endereco, String telefone, String cnh) {
-        this.nome = nome;
-        this.rg = rg;
-        this.endereco = endereco;
-        this.telefone = telefone;
-        this.cnh = cnh;
+
+    public boolean estaAptoParaAlugarCarro() {
+        if (this.getIdade() < 18) {
+            return false;
+        }
+        return true;
     }
 
-    /**
-     * Verifica se o cliente está apto para alugar um carro com base na idade.
-     * @return true se o cliente está apto, false caso contrário.
-     */
-    public boolean estaAptoParaAlugarCarro() {
-        return this.getIdade() >= 18;
+    public void registrarCliente() {
+        String caminho = new String("dadosCliente" + File.separator + "cliente_" + this.nome);
+        try {
+            FileWriter arquivo = new FileWriter(caminho, true); // se não existe, cria
+            BufferedWriter escritor = new BufferedWriter(arquivo);
+
+            escritor.write(this.nome);
+            escritor.write("\n" + this.rg);
+            escritor.write("\n" + this.cnh);
+            escritor.write("\n" + this.telefone);
+            escritor.write("\n" + this.endereco);
+            escritor.write("\n" + this.idade);
+            escritor.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
