f8d9514f49b801cbbebb214290ac797d97fc11eb
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-21 18:59:39 -0400

    Update Multa.java

--- ProjetoFinal/src/Multa.java
+++ ProjetoFinal/src/Multa.java
@@ -1,106 +1,52 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.*;
 
 public class Multa {
-    private static int contadorMulta = 1; // Contador para gerar IDs únicos para cada multa
+    private static int contadorMulta = 0;
     private int id;
     private int valor;
     private Cliente cliente;
     private Carro carro;
-    private String cnh;
     private String descricao;
 
-    /**
-     * Obtém o valor da multa.
-     * @return O valor da multa.
-     */
+    public Multa(int valor, Cliente cliente, Carro carro, String descricao) {
+        this.id = ++contadorMulta; // Incrementa o contador para obter um novo ID
+        this.valor = valor;
+        this.cliente = cliente;
+        this.carro = carro;
+        this.descricao = descricao;
+    }
+
     public int getValor() {
         return valor;
     }
 
-    /**
-     * Define o valor da multa.
-     * @param valor O novo valor da multa.
-     */
     public void setValor(int valor) {
         this.valor = valor;
     }
 
-    /**
-     * Obtém o número da CNH associado à multa.
-     * @return O número da CNH.
-     */
-    public String getCnh() {
-        return cnh;
-    }
-
-    /**
-     * Define o número da CNH associado à multa.
-     * @param cnh O novo número da CNH.
-     */
-    public void setCnh(String cnh) {
-        this.cnh = cnh;
-    }
-
-    /**
-     * Obtém o ID único da multa.
-     * @return O ID da multa.
-     */
-    public int getId() {
+    public int getId(){
         return id;
     }
-
-    /**
-     * Define o ID da multa.
-     * @param id O novo ID da multa.
-     */
-    public void setId(int id) {
+    public void setId(int id){
         this.id = id;
     }
 
-    /**
-     * Registra uma nova multa com os dados fornecidos e salva no arquivo.
-     * @param id O ID único da multa.
-     * @param valor O valor da multa.
-     * @param cnh O número da CNH associado à multa.
-     * @param cliente O cliente associado à multa.
-     * @param carro O carro associado à multa.
-     * @param descricao A descrição da multa.
-     */
-    public void registrarMulta(int id, int valor, String cnh, Cliente cliente, Carro carro, String descricao) {
-        this.id = id;
-        this.valor = valor;
-        this.cnh = cnh;
-        this.cliente = cliente;
-        this.carro = carro;
-        this.descricao = descricao;
-        salvarDadosEmArquivo();
-    }
-
-    /**
-     * Cancela uma multa com base no ID fornecido e remove os dados do arquivo.
-     * @param id O ID da multa a ser cancelada.
-     */
     public void cancelarMulta(int id) {
         removerDadosDoArquivo(id);
         System.out.println("Multa cancelada com sucesso!");
     }
 
-    /**
-     * Salva os dados da multa em um arquivo.
-     */
-    private void salvarDadosEmArquivo() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("dados_multas.txt", true))) {
+    public void registrarMulta() {
+        String caminho = new String("dadosMultas" + File.separator + "multa_" + this.id);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminho, true))) {
             // Adiciona os dados ao arquivo
-            writer.write("ID: " + id + "\n");
             writer.write("Cliente: " + cliente.getNome() + "\n");
             writer.write("Carro: " + carro.getModelo() + "\n");
-            writer.write("CNH: " + cnh + "\n");
-            writer.write("Descrição da Multa: " + descricao + "\n");
-            writer.write("Valor da Multa: " + valor + "\n");
+            writer.write("CNH: " + cliente.getCnh() + "\n");
+            writer.write("Descrição da Multa: " + this.descricao + "\n");
+            writer.write("Valor da Multa: " + this.valor + "\n");
             writer.write("\n");  // Adiciona uma linha em branco para separar as multas
+            writer.close();
 
             System.out.println("Dados salvos no arquivo com sucesso!");
         } catch (IOException e) {
@@ -108,10 +54,6 @@ public class Multa {
         }
     }
 
-    /**
-     * Remove os dados da multa do arquivo com base no ID fornecido.
-     * @param id O ID da multa a ser removida.
-     */
     private void removerDadosDoArquivo(int id) {
         try {
             // Lê o conteúdo do arquivo
@@ -141,7 +83,7 @@ public class Multa {
 
             System.out.println("Dados removidos do arquivo com sucesso!");
         } catch (IOException e) {
-            System.err.println("Erro ao remover os dados do arquivo: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
