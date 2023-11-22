e0ef1e9619c6420c9e56c697ad412ba5dfae4284
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-21 21:22:59 -0400

    Update Funcionario.java

--- ProjetoFinal/src/Funcionario.java
+++ ProjetoFinal/src/Funcionario.java
@@ -1,71 +1,96 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Funcionario {
-    // Atributos privados para armazenar informações do funcionário
     private String nome;
     private String rg;
     private String endereco;
     private String telefone;
+    private String cargo;
 
-    /**
-     * Obtém o nome do funcionário.
-     * @return O nome do funcionário.
-     */
-    public String getNome() {
-        return nome;
+    public Funcionario(String nome, String rg, String endereco, String telefone, String cargo) {
+        this.nome = nome;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.telefone = telefone;
+        this.cargo = cargo;
     }
 
-    /**
-     * Define o nome do funcionário.
-     * @param nome O novo nome do funcionário.
-     */
-    public void setNome(String nome) {
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
         this.nome = nome;
     }
 
-    /**
-     * Obtém o RG do funcionário.
-     * @return O RG do funcionário.
-     */
-    public String getRg() {
+    public String getRg(){
         return rg;
     }
-
-    /**
-     * Define o RG do funcionário.
-     * @param rg O novo RG do funcionário.
-     */
-    public void setRg(String rg) {
+    public void setRg(String rg){
         this.rg = rg;
     }
 
-    /**
-     * Obtém o endereço do funcionário.
-     * @return O endereço do funcionário.
-     */
-    public String getEndereco() {
+    public String getEndereco(){
         return endereco;
     }
-
-    /**
-     * Define o endereço do funcionário.
-     * @param endereco O novo endereço do funcionário.
-     */
-    public void setEndereco(String endereco) {
+    public void setEndereco(String endereco){
         this.endereco = endereco;
     }
 
-    /**
-     * Obtém o telefone do funcionário.
-     * @return O telefone do funcionário.
-     */
-    public String getTelefone() {
+    public String getTelefone(){
         return telefone;
     }
-
-    /**
-     * Define o telefone do funcionário.
-     * @param telefone O novo telefone do funcionário.
-     */
-    public void setTelefone(String telefone) {
+    public void setTelefone(String telefone){
         this.telefone = telefone;
     }
+
+    public String getCargo(){
+        return cargo;
+    }
+
+    public void setCargo(String cargo){
+        this.cargo = cargo;
+    }
+
+    public void registrarFuncionario() {
+        String caminho = new String("dadosFuncionario" + File.separator + "funcionario_" + this.nome);
+        try {
+            FileWriter arquivo = new FileWriter(caminho, true); // se não existe, cria
+            BufferedWriter escritor = new BufferedWriter(arquivo);
+
+            escritor.write(this.nome);
+            escritor.write("\n" + this.rg);
+            escritor.write("\n" + this.endereco);
+            escritor.write("\n" + this.telefone);
+            escritor.write("\n" + this.cargo);
+
+
+            escritor.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void excluirDadosDoArquivo() {
+        String caminho = new String("dadosFuncionario" + File.separator + "funcionario_" + this.nome);
+        try {
+            File arquivo = new File(caminho);
+
+            if (arquivo.exists()) {
+                if (arquivo.delete()) {
+                    System.out.println("Dados removidos do arquivo com sucesso!");
+                } else {
+                    System.err.println("Erro ao remover o arquivo.");
+                }
+            } else {
+                System.out.println("Arquivo não encontrado.");
+            }
+        } catch (Exception e) {
+            System.err.println("Erro ao excluir dados do arquivo: " + e.getMessage());
+        }
+    }
+
 }
