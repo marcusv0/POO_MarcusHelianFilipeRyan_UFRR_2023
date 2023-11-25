1f208392deec4a0b2deab364a25c4f23fcaf2692
Filipetom04 <142317897+Filipetom04@users.noreply.github.com>
2023-11-25 16:34:38 -0400

    Criação da classe base  Pesssoa
    Pessoa é a classe base que contém atributos comuns a ambas as classes Funcionario e Cliente, como nome, rg, endereco e telefone.
    A classe Pessoa possui um construtor que inicializa esses atributos com os valores passados como parâmetros.

---
+++ ProjetoFinal/src/Pessoa.java
@@ -0,0 +1,46 @@
+
+public class Pessoa {
+    private String nome;
+    private String rg;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(String nome, String rg, String endereco, String telefone) {
+        this.nome = nome;
+        this.rg = rg;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
