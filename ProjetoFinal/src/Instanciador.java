c29c1050c4427b4b25bd4edf5949f19779ed7ffc
saggita1 <ryanpimentel52@gmail.com>
2023-11-25 13:50:27 -0400

    Classe Instanciador
    essa classe tem o método estático lerArquivo, esse é o primeiro passo, em sua forma final ela irá ler arquivos txt e instanciar objetos em qualquer uma das classes

---
+++ ProjetoFinal/src/Instanciador.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+
+public class Instanciador {
+    public static void lerArquivo(String caminho) {
+        // Usando try-with-resources para garantir o fechamento adequado do BufferedReader
+        try (BufferedReader leitortxt = new BufferedReader(new FileReader(caminho))) {
+
+            String linha;
+
+            // ler cada linha do aquivo
+            while ((linha = leitortxt.readLine()) != null) {
+                System.out.println(linha);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
