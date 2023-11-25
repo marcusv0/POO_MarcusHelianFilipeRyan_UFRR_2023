f02221174632663b249b821f3bcebab2469ed666
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-25 16:12:09 -0400

    Update Reserva.java
    A classe Reserva agora utiliza o pacote java.time no lugar da classe Data para determinar dataInicio e dataTermino da reserva, assim como para calcular a diferença em dias.

--- ProjetoFinal/src/Reserva.java
+++ ProjetoFinal/src/Reserva.java
@@ -2,15 +2,18 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Reserva {
     private Cliente cliente;
-    private Data dataInicio;
-    private Data dataTermino;
+    private LocalDate dataInicio;
+    private LocalDate dataTermino;
     private Carro carro;
     private double valorTotal;
 
-
-    public Reserva(Cliente cliente, Data dataInicio, Data dataTermino, Carro carro, double valorReserva) {
+    public Reserva(Cliente cliente, LocalDate dataInicio, LocalDate dataTermino, Carro carro, double valorReserva) {
         this.cliente = cliente;
         this.dataInicio = dataInicio;
         this.dataTermino = dataTermino;
@@ -20,56 +23,29 @@ public class Reserva {
     }
 
     // Calcula a diferença de dias entre duas datas
-    public int diferencaEmDias(Data data1, Data data2) {
-
-        int diferencaDias;
-        int diferencaMeses;
-        int diferencaAnos;
-        int diferencaTotal;
-
-        if (data1.dia >= data2.dia){
-            diferencaDias = data1.dia - data2.dia;
-        }
-        else {
-            diferencaDias = data2.dia - data1.dia;
-        }
-
-        if (data1.mes >= data2.mes){
-            diferencaMeses = data1.mes - data2.mes;
-        }
-        else {
-            diferencaMeses = data2.mes - data1.mes;
-        }
-
-        if (data1.ano >= data2.ano){
-            diferencaAnos = data1.ano - data2.ano;
-        }
-        else {
-            diferencaAnos = data2.ano - data1.ano;
-        }
-
-        diferencaTotal = diferencaDias + (diferencaMeses * 30) + (diferencaAnos * 365);
-
-        return diferencaTotal;
+    public long diferencaEmDias(LocalDate data1, LocalDate data2) {
+        return Duration.between(data1.atStartOfDay(), data2.atStartOfDay()).toDays();
     }
 
-    public double getValorTotal(){
+    public double getValorTotal() {
         return valorTotal;
     }
 
     public void salvarReservaEmArquivo() {
-        String caminho = new String("dadosReserva" + File.separator + "reserva_" + cliente.getNome() + "_" + carro.getModelo());
+        String caminho = "dadosReserva" + File.separator + "reserva_" + cliente.getNome() + "_" + carro.getModelo();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminho, true))) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
             writer.write("Cliente: " + cliente.getNome());
             writer.newLine();
-            writer.write("Data de Início: " + dataInicio.toString());
+            writer.write("Data de Início: " + dataInicio.format(formatter));
             writer.newLine();
-            writer.write("Data de Término: " + dataTermino.toString());
+            writer.write("Data de Término: " + dataTermino.format(formatter));
             writer.newLine();
             writer.write("Carro: " + carro.getModelo());
             writer.newLine();
             writer.write("Valor total da reserva: " + this.valorTotal);
-            writer.close();
+            writer.newLine();
 
             System.out.println("Reserva salva em arquivo com sucesso.");
         } catch (IOException e) {
@@ -78,7 +54,7 @@ public class Reserva {
     }
 
     public void excluirDadosDoArquivo() {
-        String caminho = new String("dadosReserva" + File.separator + "reserva_" + cliente.getNome() + "_" + carro.getModelo());
+        String caminho = "dadosReserva" + File.separator + "reserva_" + cliente.getNome() + "_" + carro.getModelo();
         try {
             File arquivo = new File(caminho);
 
@@ -96,4 +72,3 @@ public class Reserva {
         }
     }
 }
-
