994576c7bef81e9fa7f187e329b1cf887d51b3ec
Marcus Vinicius <93393783+marcusv0@users.noreply.github.com>
2023-11-25 20:38:43 -0400

    Update Main.java
    Agora caso o cliente seja menor de idade, ele não será registrado.

--- ProjetoFinal/src/Main.java
+++ ProjetoFinal/src/Main.java
@@ -3,38 +3,66 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args){
         Locadora locadora = new Locadora("Locadora A", "123456789", "Rua Principal", "987654321");
+        
         Funcionario funcionario1 = new Funcionario("Emerson", "45812-9", "Av. Brasil", "(95) 99178-6549", "Atendente");
         locadora.contratarFuncionario(funcionario1);
+        
         Cliente cliente1 = new Cliente("André", "25539", "Av. Venezuela", "95-99948-7633", "962488", 23);
-        cliente1.registrarDados();
+        try {
+            if (cliente1.estaAptoParaAlugarCarro()) {
+                cliente1.registrarDados();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+       
         Carro carro1 = new Carro("ModeloXYZ", "ABC123", "MarcaXYZ");
         carro1.registrarDados();
+       
         Multa multa1 = new Multa(150, cliente1, carro1, "Excesso de velocidade");
         multa1.registrarDados();
+        
         LocalDate data1 = LocalDate.of(2022, 1, 1);
         LocalDate data2 = LocalDate.of(2022, 1, 8);
+        
         Reserva reserva1 = new Reserva(cliente1, data1, data2, carro1, 200);
         reserva1.registrarDados();
+
         Faturas fatura1 = new Faturas(cliente1, reserva1, multa1, carro1);
         fatura1.registrarDados();
+       
         
+
         Funcionario funcionario2 = new Funcionario("Maria", "222", "Avenida dos Trabalhadores", "123-456-789", "Gerente");
         locadora.contratarFuncionario(funcionario2);
-        Cliente cliente2 = new Cliente("Pedro", "8539", "Av. Ville Roy", "98-9948-7633", "712488", 30);
-        cliente2.registrarDados();
+        
+        Cliente cliente2 = new Cliente("Pedro", "8539", "Av. Ville Roy", "98-9948-7633", "712488", 17);
+        try {
+            if (cliente2.estaAptoParaAlugarCarro()) {
+                cliente2.registrarDados();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
         Carro carro2 = new Carro("ModelYZX", "DEF456", "MarcaYZX");
         carro2.registrarDados();
+       
         Multa multa2 = new Multa(350, cliente2, carro2, "Atravessar sinal vermelho");
         multa2.registrarDados();
+       
         LocalDate data3 = LocalDate.of(2022, 11, 10);
         LocalDate data4 = LocalDate.of(2022, 11, 17);
+       
         Reserva reserva2 = new Reserva(cliente2, data3, data4, carro2, 300);
         reserva2.registrarDados();
+       
         Faturas fatura2 = new Faturas(cliente2, reserva2, multa2, carro2);
         fatura2.registrarDados();
+        
         locadora.registrarDados();
 
-        
+        Leitor.lerPasta("dadosCliente");
         Leitor.lerPasta("dadosFuncionario");
         Leitor.lerPasta("dadosLocadora");
 
